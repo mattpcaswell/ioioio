1f41646a64b5965f4ea7187642d8b24d905f2535
Matt Caswell <mattpcaswell@gmail.com>
2017-12-21 17:18:03 -0500

    changed port to 5000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.port || 8080;
+const port = process.env.port || 5000;
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')))
 app.use('/deps', express.static(path.join(__dirname, 'node_modules')))
