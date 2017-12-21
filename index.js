01f4587d9e0d59d63c26c19f9caba86424537028
Matt Caswell <mattpcaswell@gmail.com>
2017-12-21 17:01:56 -0500

    added pixi.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 const port = process.env.port || 8080;
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')))
+app.use('/deps', express.static(path.join(__dirname, 'node_modules')))
+
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 app.listen(port, () => console.log('Listening on port ' + port));
