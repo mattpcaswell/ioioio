63ba662bad6a62ba9681fbc89d4ae4e4b12b669e
Matt Caswell <mattpcaswell@gmail.com>
2017-12-21 16:38:29 -0500

    added js and css

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = process.env.port || 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use('/assets', express.static(path.join(__dirname, 'assets')))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
-app.listen(port, () => console.log('Example app listening on port ' + port + '!'));
+app.listen(port, () => console.log('Listening on port ' + port));
